91f5b5ae8372dcd9b2b92ed910fd9f97b1029450
Loïc SENCE <senceloic@gmail.com>
2018-11-22 09:49:14 +0100

    Add PushNotificationManager

---
+++ PushNotificationManager.swift
@@ -0,0 +1,31 @@
+//
+//  PushNotificationManager.swift
+//  Howdy
+//
+//  Created by Loic SENCE on 30/07/2018.
+//  Copyright © 2018 Howdy. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@available(iOS 10.0, *)
+struct PushNotificationManager {
+    
+    static func registerForPushNotifications() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            print("Permission granted: \(granted)")
+        }
+    }
+    
+    func notify(text: String) {
+        let content = UNMutableNotificationContent()
+        content.body = text
+        content.title = "Location posted"
+        content.sound = nil
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    
+}
